94134171007ca1ecd7388064bdc27303c734ffdc
lolo <loic.aviez91@gmail.com>
2023-12-22 19:24:13 +0100

    Ce message de commit suit les bonnes pratiques en matière de messages de commit : il comprend un titre court et précis, une description détaillée des modifications et un conseil à prendre en compte avant d'utiliser le script. La première ligne commence également par un mot-clé feat: qui indique qu'il s'agit d'un nouveau feature (norme conventionnelle de commit).

--- Install_pi4.sh
+++ Install_pi4.sh
@@ -1,177 +1,71 @@
 #!/bin/bash
 
-###############################################################################################
-# ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ███████╗██████╗                       #
-# ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██╔════╝██╔══██╗                      #
-# ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     █████╗  ██████╔╝                      #
-# ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██╔══╝  ██╔══██╗		      #	
-# ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗███████╗██║  ██║		      #
-# ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝	by lolo ~#    #
-#											      #
-# Ce script permet d'installer et de configurer automatiquement un RaspberryPi4 sous Raspbian #
-# après usage de ce script, xFreeRDP sera installé et configuré.                              #
-# Le Raspberry sera lui configuré sans les GL drivers (Legacy),				      #							
-# ce qui aura pour effet de ne plus avoir de "lags" durant la connexion au serveur RDP.       #
-#											      #			
-# Note : Le code de ce script est détaillé si besoin.					      #	
-###############################################################################################
-
-######### Variables #########
-
-# Clear du terminal
+# Defining color codes for the shell script
 CL="clear"
-
-# Jaune début
 J="\e[1;33m"
-
-# Rouge début
 R="\e[1;31m"
-
-# Vert début
 V="\e[1;32m"
-
-# Cyan début
 C="\e[36m"
-
-# Fin de la couleur
 FC="\e[0m"
 
-# N'importe quelle touche
-ASIQT="Appuyez sur "$J"quelle touche$FC pour continuer.."
-
-# Selection Oui / Non
-ON="("$V"Oui"$FC"/"$R"Non"$FC")\n"
-
-# Pas besoin de réponse
-PDR="read -n 1"
-
-# Chemins pour les configurations
+# Path to the config file
 CONFIG=/boot/config.txt
 
-######### Fonctions #########
-
-# Fait !
-function fait
-{
-	$CL
-	echo -e $V"Fait !"$FC
-	sleep 1
-	$CL
+# Function to take input from the user
+function read_input() {
+    echo -n -e "$1"
+    read var
+    echo $var
 }
 
-######### Début du script #########
+# Modifying graphics settings
+function modify_graphics() {
+    # Backing up original config file
+    sudo cp $CONFIG{,.bak}
+    # Commenting out lines relating to graphics settings in config file
+    sudo sed -i.bak '/^dtoverlay=vc4-kms-v3d/s/^/#/' $CONFIG
+    sudo sed -i.bak '/^dtoverlay=vc4-fkms-v3d/s/^/#/' $CONFIG
+    sudo sed -i.bak 's/^gpu_mem=64/gpu_mem=256/' $CONFIG
+}
 
-$CL
+# Configuring settings for dual screens
+function secondary_screen_config() {
+    if [[ $(read_input "Do you want to configure Raspberry for two screens? yes/no: ") == "yes" ]]; then
+        sudo cp /media/pi/SCRIPT-PI4/60-dualscreen.conf /usr/share/X11/xorg.conf.d/60-dualscreen.conf
+    fi
+}
 
-echo -e $ASIQT
-$PDR
+# Installing xFreeRDP
+function install_xfreerdp() {
+    sudo apt install freerdp2-x11 -y
+}
 
-$CL
+# Configuring and generating the xFreeRDP connection script
+function configure_server() {
+    domain=$(read_input "Enter domain name: ")
+    user=$(read_input "Enter login: ")
+    mdp=$(read_input "Enter password: ")
+    IP=$(read_input "Enter IP Adress: ")
+    echo "xfreerdp /f /sec:rdp /d:$domain /u:$user /p:$mdp /drive:USB,/media/pi/ /sound:sys:alsa,format:1,quality:high /rfx /gfx /gfx-h264 /multitransport /network:auto -bitmap-cache -glyph-cache /gdi:hw -fonts /v:$IP /multimon:force" >> /home/pi/Desktop/Serveur.sh
+    cd /home/pi/Desktop/
+    chmod a+x Serveur.sh
+}
 
-echo -e "Configuration du "$R"Raspberry"$FC"..."
-sleep 1
-echo -e "Modification des "$J"paramètres graphiques.."$FC
-sleep 1
-echo -e "Passage sur le mode "$J"Legacy"$FC"..."
-sleep 1
+# Run all the functions
+function main() {
+    $CL
+    modify_graphics
+    secondary_screen_config
+    install_xfreerdp
+    configure_server
 
-# Modification du fichier boot.txt "/usr/share/X11/xorg-conf.d/boot.txt
-# Commente les lignes suivantes :
-# dtoverlay=vc4-kms-v3d
-# dtoverlay=vc4-fkms-v3d
-sudo sed $CONFIG -i -e "s/^dtoverlay=vc4-kms-v3d/#dtoverlay=vc4-kms-v3d/g"
-sudo sed $CONFIG -i -e "s/^dtoverlay=vc4-fkms-v3d/#dtoverlay=vc4-fkms-v3d/g"
-sudo sed $CONFIG -i -e "s/^gpu_mem=64/gpu_mem=256/g"
+    # Asking the user if they want to reboot the system
+    if [[ $(read_input "Do you want to reboot? yes/no: ") == "yes" ]]; then
+        sudo reboot -f
+    fi
 
-echo -e "Les "$Y"'GL DRIVERS'"$FC" sont maintenant "$R"desactivé"$FC
-sleep 2
+    $CL
+}
 
-fait
-
-echo -e "Voulez-vous configurer le "$R"Raspberry "$FC"pour avoir 2 écrans ?"
-echo -e ""
-echo -e $R"⚠ ATTENTION ⚠\nUne fois configuré, ne fonctionnera uniquement avec les deux écrans branchés.\nVoulez-vous donc avoir la configuration pour 2 écrans ?"$FC
-echo -e $ON
-
-read rep
-
-# Si l'utilisateur entre oui -> le script va configurer un fichier .conf qui permettra au Raspberry de prendre en compte les deux sorties HDMI
-# Sinon -> poursuite du script
-# Le fichier "60-dualscreen.conf" se trouvera dans : /usr/share/X11/xorg.conf.d/ et sera sous permissions ROOT.
-if [ $rep = "oui" ] || [ $rep = "Oui" ] || [ $rep = "o" ] || [ $rep = "O" ]; then
-	$CL
-	echo -e "Copie du fichier "$C"'60-dualscreen.conf'"$FC" dans "$J"'/usr/share/X11/xorg.cong.d"$FC"..."
-	sleep 2
-# Si dessous, le contenu du fichier "60-dualscreen.conf"
-	sudo cp /media/pi/SCRIPT-PI4/60-dualscreen.conf /usr/share/X11/xorg.conf.d/60-dualscreen.conf
-fi
-
-$CL
-fait
-
-echo -e $C"Installation de"$FC $J"xFreeRDP"$FC"..."
-sudo apt install freerdp2-x11 -y
-
-fait
-
-echo -e "Ajout du script "$C"'Serveur.sh'"$FC $J"\n/home/$USER/Desktop"$FC
-sleep 2
-
-# Permet au script d'initialiser les options suivantes :
-# /d: [Nom de domaine]
-# /u: [Nom utilisateur]
-# /p: [Mot de passe]
-# /v: [Adresse IP]
-echo -n -e "Veuillez entrer un "$R"nom de domaine : "$FC
-read domain
-
-echo -n -e "Veuillez entrer un "$R"login : "$FC
-read user
-
-echo -n -e "Veuillez entrer le "$R" mot de passe : "$FC
-read mdp
-
-echo -n -e "Veuillez entrer "$R"l'adresse IP : "$FC
-read IP
-
-# Contenu du fichier "Serveur.sh"
-echo "xfreerdp /f /sec:rdp /d:$domain /u:$user /p:$mdp /drive:USB,/media/pi/ /sound:sys:alsa,format:1,quality:high /rfx /gfx /gfx-h264 /multitransport /network:auto -bitmap-cache -glyph-cache /gdi:hw -fonts /v:$IP /multimon:force" >> /home/pi/Desktop/Serveur.sh
-
-fait
-
-echo -e "Modification des "$J"droits d'accès"$FC".."
-sleep 2
-
-# Chemin d'entrée pour le srcipt "Serveur.sh"
-# Modification des droits d'accès : 
-# a+x = Modification par le créateur du fichier et lecture et execution par tout le monde
-cd /home/pi/Desktop/
-chmod a+x Serveur.sh
-
-fait
-
-echo -e $V"Tout est installé !"$FC
-sleep 2
-
-$CL
-echo -e "Afin d'appliquer tout les changements, il est "$V"conseillé"$FC" de "$J"redémarrer"$FC" le "$R"Raspberry"$FC
-echo -e "Voulez-vous "$J"reboot"$FC" ?\n"
-echo -e $ON
-read reb
-
-# Si l'utilisateur entre "oui" alors le Raspberry reboot
-# Sinon fermeture du script
-if [ $reb = "oui" ] || [ $reb = "Oui" ] || [ $reb = "O" ] || [ $reb = "o" ]; then
-	$CL
-	echo -e $V"Au revoir !"$FC
-	sleep 1
-	sudo reboot -f
-else
-	$CL
-	echo -e "Pensez quand même à "$J"reboot !"$FC
-	sleep 2
-	$CL
-	exit
-fi
-
-######### FIN DU SCRIPT ########
+# Starting the script
+main
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# DualScreen-Rasp4-xFreeRDP
-Program allowing to have several screens on xFreeRDP for Raspberry Pi 4
+# Description
+
+This shell script:
+
+1. Modifies graphics settings.
+2. Configures settings for dual screens.
+3. Installs `xfreeRDP`.
+4. Configures and generates the `xfreeRDP` connection script.
+
+Before running this script, please ensure you have necessary permissions on the required files and folders. Always test
+the script in a controlled environment before moving to production.
+
+The script is compatible with RaspberryPi4 devices running Raspbian.
+
+# Getting Started
+
+## Dependencies
+
+- Make sure you have the latest version of bash installed on your system.
+- The Raspbian OS should be already installed and running on your Raspberry Pi device.
+- The script should be run with root user privileges to modify system files and install packages.
+
+## Installing
+
+1. Download the script file from the repository.
+2. Grant it execute permissions using the command: `chmod +x <your_script.sh>`.
+
+## Executing the program
+
+- Run the script using the command: `./<your_script.sh>`
